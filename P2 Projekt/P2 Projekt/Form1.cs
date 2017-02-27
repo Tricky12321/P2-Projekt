7ee4a96ebd6b71c000a782ea6de8cb18a10ed3d6
Thue Iversen <Thue-V-Iversen@hotmail.com>
2017-02-27 11:13:15 +0100

    Lavede en test knap

--- P2 Projekt/P2 Projekt/Form1.cs
+++ P2 Projekt/P2 Projekt/Form1.cs
@@ -15,5 +15,10 @@ namespace P2_Projekt
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dette er en test!");
+        }
     }
 }
